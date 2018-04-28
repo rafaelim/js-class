6fa00df2c7a4c9bd57c7a2b7d854f3b4a66d9cc1
Rafael Lima <lima@localhost.localdomain>
2018-04-28 08:37:57 -0400

    consume api

--- first-class/exercice-1/day-quote.js
+++ first-class/exercice-1/day-quote.js
@@ -1,3 +1,8 @@
+var _URI = "http://api.forismatic.com/api/1.0/";
+var params = '?method=getQuote&lang=en&format=jsonp&jsonp=?';
 $("#button").on('click', function() {
-    console.log('teahdua');
+    $.getJSON(_URI + params, function(res) {
+        $("#quote").text(res.quoteText);
+        $("#quote-author").text(res.quoteAuthor);
+    });
 });
